7908d13c44c3254788bafe93851ab7a60c7fab7a
Jane Le <janehtle@gmail.com>
2025-07-27 19:28:00 -0400

    event listener added

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ function displayInput(input) {
     display.value += input;
 }
 
-//function assigned to onclick event handler and will return empty string in display when clicked
+//add event listener where you can type the operands and operator instead of clicking btns on screen
+document.addEventListener("keydown", (event) => { //keydown, keypress, keyup - keydown better
+    // console.log(event);
+    const keys = event.key; //regex here didn't work, event.key will return string of key pressed
+
+    if (/[\d.+\-*/]/.test(keys)) { //use .test for regular exp, .includes for comparing values against values
+        displayInput(keys); //ref function
+    } else if (keys === "Backspace") {
+        display.value = display.value.slice(0, -1); //will splice/delete last character
+    }
+});
+
+//func will return empty string in input field
 function clearDisplay() {
     display.value = "";
 }
 
-//function assigned to onclick event handler and will calculate what is present in the display
+//func that will calculate whats displayed in input field
 function calculate() {
     try {
-        display.value = eval(display.value); //not good practice to use eval() alone so pass it in try catch statement for error handling
+        display.value = eval(display.value); //not good practice to use eval() alone so pass in try catch statement for error handling
     } catch {
-        display.value = "404"; //will display 404 aka error if the result of input will be invalid
+        display.value = "404"; 
     }
 }
+
+
